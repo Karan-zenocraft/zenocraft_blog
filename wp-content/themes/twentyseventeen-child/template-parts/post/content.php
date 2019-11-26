109e9cd6502a84f5a1328da20d69d783877facc6
root <root@localhost.creatusello.com>
2019-11-26 11:46:55 +0530

    blog page add title to the page

--- wp-content/themes/twentyseventeen-child/template-parts/post/content.php
+++ wp-content/themes/twentyseventeen-child/template-parts/post/content.php
@@ -12,68 +12,69 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID();?>" <?php post_class();?>>
 	<?php
-	if ( is_sticky() && is_home() ) :
-		echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
-	endif;
-	?>
+if (is_sticky() && is_home()):
+    echo twentyseventeen_get_svg(array('icon' => 'thumb-tack'));
+endif;
+?>
 	<header class="entry-header">
 		<?php
-		if ( 'post' === get_post_type() ) {
-			echo '<div class="entry-meta">';
-			if ( is_single() ) {
-				twentyseventeen_posted_on();
-			} else {
-				echo twentyseventeen_time_link();
-				twentyseventeen_edit_link();
-			};
-			echo '</div><!-- .entry-meta -->';
-		};
+if ('post' === get_post_type()) {
+    echo '<div class="entry-meta">';
+    if (is_single()) {
+        twentyseventeen_posted_on();
+    } else {
+        echo get_the_title();
+        twentyseventeen_edit_link();
+    }
+    ;
+    echo '</div><!-- .entry-meta -->';
+}
+;
 
-		
-		?>
+?>
 	</header>
-    
-    
-    
+
+
+
     <!-- .entry-header -->
 
-	<?php if ( '' !== get_the_post_thumbnail() && ! is_single() ) : ?>
+	<?php if ('' !== get_the_post_thumbnail() && !is_single()): ?>
 		<div class="post-thumbnail">
-			<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>
+			<a href="<?php the_permalink();?>">
+				<?php the_post_thumbnail('twentyseventeen-featured-image');?>
 			</a>
 		</div>
-    
+
     <!-- .post-thumbnail -->
-	<?php endif; ?>
+	<?php endif;?>
 
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				/* translators: %s: Post title. */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
-				get_the_title()
-			)
-		);
+the_content(
+    sprintf(
+        /* translators: %s: Post title. */
+        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen'),
+        get_the_title()
+    )
+);
 
-		wp_link_pages(
-			array(
-				'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
-				'after'       => '</div>',
-				'link_before' => '<span class="page-number">',
-				'link_after'  => '</span>',
-			)
-		);
-		?>
+wp_link_pages(
+    array(
+        'before' => '<div class="page-links">' . __('Pages:', 'twentyseventeen'),
+        'after' => '</div>',
+        'link_before' => '<span class="page-number">',
+        'link_after' => '</span>',
+    )
+);
+?>
 	</div><!-- .entry-content -->
 
 	<?php
-	if ( is_single() ) {
-		twentyseventeen_entry_footer();
-	}
-	?>
+if (is_single()) {
+    twentyseventeen_entry_footer();
+}
+?>
 
-</article><!-- #post-<?php the_ID(); ?> -->
+</article><!-- #post-<?php the_ID();?> -->
