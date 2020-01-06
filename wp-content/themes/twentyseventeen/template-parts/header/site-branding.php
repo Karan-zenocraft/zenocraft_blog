07bf622fdede1b46529ce9b20b270d7284adfb86
root <root@localhost.creatusello.com>
2020-01-06 12:33:29 +0530

    add header href to logo

--- wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
+++ wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
@@ -12,34 +12,33 @@
 //$uploads = wp_upload_dir();
 //echo wp_basename( $uploads['baseurl'] );
 
-
 ?>
 <div class="site-branding">
 	<div class="wrap">
 
-		<?php the_custom_logo(); ?>
+		<?php the_custom_logo();?>
 
 		<div class="site-branding-text">
-			<?php if ( is_front_page() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-            <?php $uploads = wp_upload_dir(); ?>
-			<img src="<?php echo $uploads['baseurl'] . "/2019/11/logo.png"; ?>" alt="Zenocraft Logo" class="Logo">	
-           
-			<?php endif; ?>
+			<?php if (is_front_page()): ?>
+				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name');?></a></h1>
+			<?php else: ?>
+            <?php $uploads = wp_upload_dir();?>
+			<a href="http://zenocraft.com"><img src="<?php echo $uploads['baseurl'] . "/2019/11/logo.png"; ?>" alt="Zenocraft Logo" class="Logo"></a>
+
+			<?php endif;?>
 
 			<?php
-			$description = get_bloginfo( 'description', 'display' );
+$description = get_bloginfo('description', 'display');
 
-			if ( $description || is_customize_preview() ) :
-				?>
-				
-			<?php endif; ?>
+if ($description || is_customize_preview()):
+?>
+
+			<?php endif;?>
 		</div><!-- .site-branding-text -->
 
-		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
-		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
-	<?php endif; ?>
+		<?php if ((twentyseventeen_is_frontpage() || (is_home() && is_front_page())) && !has_nav_menu('top')): ?>
+		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg(array('icon' => 'arrow-right')); ?><span class="screen-reader-text"><?php _e('Scroll down to content', 'twentyseventeen');?></span></a>
+	<?php endif;?>
 
 	</div><!-- .wrap -->
 </div><!-- .site-branding -->
